f539bf5b456e43c616cc3f29defa5171d9e27391
Artem Voloschenko <avoloschenko008@gmail.com>
2026-02-03 19:58:03 +0200

    Department getters and Consutrctor

--- src/main/java/ua/volobzh/utils/Department.java
+++ src/main/java/ua/volobzh/utils/Department.java
@@ -3,8 +3,7 @@ package ua.volobzh.utils;
 public class Department {
 
     private int ID;
-    private String Name;
-    private String location;
+    private String name;
     //private String head; Sanya there must be zav kafedry
     private String office;
 
@@ -13,5 +12,33 @@ public class Department {
 
     private Object[] teachers; //analogichno
     private int teacherCount;
+
+    public Department(int ID, String Name, String office) {
+        this.ID = ID;
+        this.name = Name;
+        this.office = office;
+
+        this.students = new Object[10];
+        this.studentCount = 0;
+        this.teachers = new Object[10];
+        this.teacherCount = 0;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    @Override
+    public String toString() {
+        return "Department" + name;
+    }
 
 }
