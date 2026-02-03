3b8ec1032bd975ad1d50ef9185a267fd4a273db8
Artem Voloschenko <avoloschenko008@gmail.com>
2026-02-03 20:10:30 +0200

    Faculty getters and Constructor

--- src/main/java/ua/volobzh/utils/Faculty.java
+++ src/main/java/ua/volobzh/utils/Faculty.java
@@ -10,4 +10,31 @@ public class Faculty {
     private Department[] departments;
     private int departmentCount;
 
+    public Faculty(int id, String fullName, String shortName, String decan, String contacts) {
+        this.id = id;
+        this.fullName = fullName;
+        this.shortName = shortName;
+        this.decan = decan;
+        this.contacts = contacts;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public String getDecan() {
+        return decan;
+    }
+
+    public String getContacts() {
+        return contacts;
+    }
 }
