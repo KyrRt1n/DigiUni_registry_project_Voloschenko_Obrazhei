39a54540911ca028817fc4b1cb88006969b94537
OleksandrObrazhei <oleksandrobrazhei@gmail.com>
2026-02-03 21:11:16 +0200

    Created person, student, and teacher classes

---
+++ src/main/java/ua/sopsany/Models/Person.java
@@ -0,0 +1,58 @@
+package ua.sopsany.Models;
+
+public abstract class Person {
+
+    private String name;
+    private String surname;
+    private String lastname;
+    private String birthday;
+    private String email;
+    private int phone;
+    private int id;
+
+
+
+        public Person(String name, String surname, String lastname, String birthday, String email, int phone, int id) {
+            this.name = name;
+            this.surname = surname;
+            this.lastname = lastname;
+            this.birthday = birthday;
+            this.email = email;
+            this.phone = phone;
+            this.id = id;
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return id + ": " + surname + " " + name + " (" + phone + ")";
+    }
+
+}
