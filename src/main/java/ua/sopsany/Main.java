de694b56550278c0c0eabd6658aba3d86f8c5165
Artem Voloschenko <avoloschenko008@gmail.com>
2026-02-03 22:40:57 +0200

    Small test of CLI, goodnight

--- src/main/java/ua/sopsany/Main.java
+++ src/main/java/ua/sopsany/Main.java
@@ -1,7 +1,25 @@
 package ua.sopsany;
 
+import ua.sopsany.models.Department;
+import ua.sopsany.models.Faculty;
+import ua.sopsany.utils.InputHandler;
+import ua.sopsany.models.University;
+
 public class Main {
     public static void main(String[] args) {
+        University university = new University("НаУКМА", "НаУКМА", "Киев", "Сковороды 2");
+        Faculty fi = new Faculty(1, "Faculty of Infomatics", "FI", "Hlybovets","044-111-22-33");
+        university.addFaculty(fi);
 
+        Department se = new Department(1, "Software Engineering", "1-225");
+        fi.addDepartment(se);
+
+        InputHandler input = new InputHandler();
+
+        while (true) {
+            System.out.println("Right now there is " + university.getFacultyCount() + " faculties");
+
+            int choice = input.readInt("Select", 1, 3);
+        }
     }
 }
