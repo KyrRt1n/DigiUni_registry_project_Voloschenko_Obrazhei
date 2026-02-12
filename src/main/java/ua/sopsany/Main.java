7f201c25f95a42dfb0d0bb797a92f7052aecad90
Artem Voloschenko <avoloschenko008@gmail.com>
2026-02-12 14:46:46 +0200

    Finder update

--- src/main/java/ua/sopsany/Main.java
+++ src/main/java/ua/sopsany/Main.java
@@ -3,13 +3,15 @@ package ua.sopsany;
 import ua.sopsany.models.*;
 import ua.sopsany.utils.InputHandler;
 import ua.sopsany.utils.Repository;
+import ua.sopsany.utils.StudentFinder;
 
 import java.time.LocalDate;
 
 public class Main {
 
-    static University university = Repository.createUniversity();
+    public static University university = Repository.createUniversity();
     static InputHandler input = new InputHandler();
+    static StudentFinder findStudent = new StudentFinder();
 
     public static void main(String[] args) {
 
@@ -17,7 +19,7 @@ public class Main {
             System.out.println("\n--- UNIVERSITY SYSTEM MENU ---");
             System.out.println("1. Print University Structure");
             System.out.println("2. Add Student");
-            System.out.println("3. Find Student by Lastname (in SE)");
+            System.out.println("3. Find Student");
             System.out.println("4. Show all students");
             System.out.println("5. Remove Student");
             System.out.println("0. Exit");
@@ -34,7 +36,7 @@ public class Main {
                     break;
 
                 case 3:
-                    findStudentByLastname();
+                    findStudent();
                     break;
 
                 case 4:
@@ -58,7 +60,7 @@ public class Main {
 
     private static void studentUpdate() {
         System.out.println("=== Student info updater ===");
-        String lastnameToUpd = input.readString("Lastname: ");
+        String lastnameToUpd = input.readString("Lastname");
         Student foundStudent = null;
         for (Faculty f : university.getFaculties()) {
             for (Department d : f.getDepartments()) {
@@ -86,10 +88,10 @@ public class Main {
         int choiceToUpd = input.readInt("Select option", 0, 2);
         switch (choiceToUpd) {
             case 1:
-                foundStudent.setCourse(input.readInt("Course: ", 1, 6));
+                foundStudent.setCourse(input.readInt("Course", 1, 6));
                 break;
             case 2:
-                foundStudent.setGroup(input.readString("Group: "));
+                foundStudent.setGroup(input.readString("Group"));
                 break;
             case 0:
                 System.out.println("Update cancelled");
@@ -104,7 +106,7 @@ public class Main {
         for (Faculty f : university.getFaculties()) {
             System.out.println("  └Faculty: " + f);
             for (Department d : f.getDepartments()) {
-                System.out.println("  └-Department: " + d.getName());
+                System.out.println("   └-Department: " + d.getName());
             }
         }
         System.out.println("============");
@@ -144,20 +146,33 @@ public class Main {
         System.out.println("Student added successfully!");
     }
 
-    private static void findStudentByLastname() {
+    private static void findStudent() {
         boolean found = false;
-        String lastnameToFind = input.readString("Enter lastname to search");
-        for (Faculty f : university.getFaculties()) {
-            for (Department d : f.getDepartments()) {
-                for (Student s : d.getStudents()) {
-                    if (s.getLastname().equalsIgnoreCase(lastnameToFind)) {
-                        System.out.println("FOUND: " + s);
-                        found = true;
-                    }
-                }
-            }
+
+        System.out.println("1. Find students in course of your choice");
+        System.out.println("2. Find students in group of your choice");
+        System.out.println("3. Find students by Name, Lastname and Surname");
+        System.out.println("0. Go back");
+        int findingOption = input.readInt("How to you wanna find student?", 0, 3);
+
+        switch (findingOption) {
+            case 1:
+                int courseToFind = input.readInt("Select course:");
+                findStudent.ByCourse(courseToFind);
+                break;
+            case 2:
+                String groupToFind = input.readString("Select group:");
+                findStudent.ByGroup(groupToFind);
+                break;
+            case 3:
+                String lastnameToFind = input.readString("Enter lastname to search");
+                String nameToFind = input.readString("Enter student name to search");
+                String surnameToFind = input.readString("Enter surname to search");
+                findStudent.ByFullName(nameToFind, lastnameToFind,  surnameToFind);
+                break;
+            case 0: return;
         }
-        if (!found) System.out.println("Student not found.");
+
     }
 
     private static void printStudentList() {
