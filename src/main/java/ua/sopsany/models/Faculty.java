487d01b7d476a6a8f78fad5cbd6100e12642d2b3
OleksandrObrazhei <oleksandrobrazhei@gmail.com>
2026-03-05 19:30:23 +0200

    Replaced arrays with lists, added lambdas

--- src/main/java/ua/sopsany/models/Faculty.java
+++ src/main/java/ua/sopsany/models/Faculty.java
@@ -1,90 +1,75 @@
-package ua.sopsany.models;
+    package ua.sopsany.models;
 
-import java.util.Arrays;
+    import java.util.ArrayList;
+    import java.util.List;
 
-public class Faculty {
-    private int id;
-    private String fullName;
-    private String shortName;
-    private Teacher decan;
-    private String contacts;
+    public class Faculty {
+        private int id;
+        private String fullName;
+        private String shortName;
+        private Teacher decan;
+        private String contacts;
 
-    private Department[] departments;
-    private int departmentCount;
 
-    public Faculty(int id, String fullName, String shortName, Teacher decan, String contacts) {
-        this.id = id;
-        this.fullName = fullName;
-        this.shortName = shortName;
-        this.decan = decan;
-        this.contacts = contacts;
-        this.departments = new Department[3];
-        this.departmentCount = 0;
+        private List<Department> departments =  new ArrayList<>();
+
+        public Faculty(int id, String fullName, String shortName, Teacher decan, String contacts) {
+            this.id = id;
+            this.fullName = fullName;
+            this.shortName = shortName;
+            this.decan = decan;
+            this.contacts = contacts;//можливо тут видалив зайве, після тесту треба перевірити
+        }
+
+        public Faculty(int id, String fullName, String shortName) {
+            this.id = id;
+            this.fullName = fullName;
+            this.shortName = shortName;
+
+        }
+
+        public void addDepartment(Department department) {
+            departments.add(department);
+        }
+
+
+        public void setDepartments(List<Department> departments) {
+            this.departments = departments;
+        }
+        public void setContacts(String contacts) {
+            this.contacts = contacts;
+        }
+
+        public void setDecan(Teacher decan) {
+            this.decan = decan;
+        }
+
+        public List<Department> getDepartments() {
+            return departments;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public String getShortName() {
+            return shortName;
+        }
+
+        public Teacher getDecan() {
+            return decan;
+        }
+
+        public String getContacts() {
+            return contacts;
+        }
+
+        @Override
+        public String toString() {
+            return shortName + " - " + fullName + " (Contacts: " + contacts + ")";
+        }
     }
-
-    public Faculty(int id, String fullName, String shortName) {
-        this.id = id;
-        this.fullName = fullName;
-        this.shortName = shortName;
-        this.departments = new Department[3];
-        this.departmentCount = 0;
-    }
-
-    public void addDepartment(Department department) {
-        if(departmentCount == departments.length)
-            departments = Arrays.copyOf(departments, departments.length + 5);
-
-
-        departments[departmentCount] = department;
-        departmentCount++;
-    }
-
-    public Department[] getDepartments() {
-        return Arrays.copyOf(this.departments, this.departmentCount);
-    }
-
-    public int getDepartmentCount() {
-        return departmentCount;
-    }
-
-    public void setDepartmentCount(int departmentCount) {
-        this.departmentCount = departmentCount;
-    }
-
-    public void setDepartments(Department[] departments) {
-        this.departments = departments;
-    }
-
-    public void setContacts(String contacts) {
-        this.contacts = contacts;
-    }
-
-    public void setDecan(Teacher decan) {
-        this.decan = decan;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getShortName() {
-        return shortName;
-    }
-
-    public Teacher getDecan() {
-        return decan;
-    }
-
-    public String getContacts() {
-        return contacts;
-    }
-
-    @Override
-    public String toString() {
-        return shortName + " - " + fullName + " (Contacts: " + contacts + ")";
-    }
-}
--- src/main/java/ua/sopsany/models/University.java
+++ src/main/java/ua/sopsany/models/University.java
@@ -1,6 +1,7 @@
 package ua.sopsany.models;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class University {
     private String fullName;
@@ -8,16 +9,14 @@ public class University {
     private String city;
     private String address;
 
-    private Faculty[] faculties;
-    private int facultyCount;
+    private List<Faculty> faculties = new ArrayList<>();
 
     public University(String fullName, String shortName, String city, String address) {
         this.fullName = fullName;
         this.shortName = shortName;
         this.city = city;
         this.address = address;
-        this.faculties = new Faculty[3];
-        this.facultyCount = 0;
+
     }
 
     public University(String fullName, String shortName) {
@@ -26,12 +25,7 @@ public class University {
     }
 
     public void addFaculty(Faculty faculty) {
-        if(facultyCount == faculties.length) {
-            faculties = Arrays.copyOf(faculties, faculties.length + 5);
-        }
-
-        faculties[facultyCount] = faculty;
-        facultyCount++;
+        faculties.add(faculty);
     }
 
     public void setCity(String city) {
@@ -42,20 +36,8 @@ public class University {
         this.address = address;
     }
 
-    public void setFaculties(Faculty[] faculties) {
-        this.faculties = faculties;
-    }
-
-    public void setFacultyCount(int facultyCount) {
-        this.facultyCount = facultyCount;
-    }
-
-    public Faculty[] getFaculties() {
-        return Arrays.copyOf(faculties, facultyCount);
-    }
-
-    public int getFacultyCount() {
-        return facultyCount;
+    public List<Faculty> getFaculties() {
+        return faculties;
     }
 
     public String getFullName() {
