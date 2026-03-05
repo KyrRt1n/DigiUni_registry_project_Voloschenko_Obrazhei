be27c7f99a50ccb7db9112f534454dde09657594
Artem Voloschenko <avoloschenko008@gmail.com>
2026-03-05 19:41:26 +0200

    SearchService

---
+++ src/main/java/ua/sopsany/utils/SearchService.java
@@ -0,0 +1,26 @@
+package ua.sopsany.utils;
+
+import ua.sopsany.models.*;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SearchService {
+
+    public List<Student> findByLastName(List<Student> students, String lastName) {
+        return students.stream().filter(s -> s.getLastname().equalsIgnoreCase(lastName)).toList();
+    }
+
+    public List<Student> findByGroup(List<Student> students, String group) {
+        return students.stream().filter(s -> s.getGroup().equalsIgnoreCase(group)).toList();
+    }
+
+    public List<Student> findByCourse(List<Student> students, int course) {
+        return students.stream().filter(s -> s.getCourse() == course).toList();
+    }
+
+    public List<Student> sortByLastname(List<Student> students) {
+        return students.stream().sorted(Comparator.comparing(Person::getLastname)).toList();
+    }
+
+}
