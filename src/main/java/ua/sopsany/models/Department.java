a364c629c7c9cfacad39b0f50b3a26719b7f5386
Artem Voloschenko <avoloschenko008@gmail.com>
2026-03-12 11:52:47 +0200

    faculty and head added

--- src/main/java/ua/sopsany/models/Department.java
+++ src/main/java/ua/sopsany/models/Department.java
@@ -1,4 +1,6 @@
 package ua.sopsany.models;
+import ua.sopsany.utils.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,16 +9,31 @@ public class Department {
     private int ID;
     private String name;
     private String office;
+    private Teacher head;
+    private Faculty faculty;
 
     private List<Student> students = new ArrayList<>();
     private List<Teacher> teachers = new ArrayList<>();
 
+    public Department(int ID, String Name, String office, Teacher head, Faculty faculty) {
+        this.ID = ID;
+        this.name = Name;
+        this.office = office;
+        this.head = head;
+        this.faculty = faculty;
+    }
+
+    public Department(int ID, String Name, String office, Teacher head) {
+        this.ID = ID;
+        this.name = Name;
+        this.office = office;
+        this.head = head;
+    }
 
     public Department(int ID, String Name, String office) {
         this.ID = ID;
         this.name = Name;
         this.office = office;
-
     }
 
     public Department(int ID, String name) {
@@ -32,10 +49,25 @@ public class Department {
         return students.removeIf(s -> s.getLastname().equals(lastname));
     }
 
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
     public void setOffice(String office) {
         this.office = office;
     }
 
+    public Teacher getHead() {
+        return head;
+    }
+
+    public void setHead(Teacher head) {
+        this.head = head;
+    }
 
     public void setStudents(List<Student> students) {
         this.students = students;
@@ -63,7 +95,8 @@ public class Department {
 
     @Override
     public String toString() {
-        return "Department " + name;
+        String headName = (head != null) ? head.getLastname() : "not assigned";
+        return "Department " + name + ", Head: " + headName;
     }
 
 }
