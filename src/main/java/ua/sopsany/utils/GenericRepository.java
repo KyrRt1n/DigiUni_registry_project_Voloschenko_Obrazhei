453c849831da47cac8bfa0edbe4475a19a95fd70
Artem Voloschenko <avoloschenko008@gmail.com>
2026-03-12 11:53:21 +0200

    exception thorws

--- src/main/java/ua/sopsany/utils/GenericRepository.java
+++ src/main/java/ua/sopsany/utils/GenericRepository.java
@@ -1,16 +1,23 @@
 package ua.sopsany.utils;
 
+import ua.sopsany.exceptions.*;
+
 import java.util.*;
 
 public class GenericRepository<T> {
-    private List<T> items = new  ArrayList<T>();
+    private List<T> items = new ArrayList<T>();
 
-    public void add(T item) {
+    public void add(T item) throws DuplicateIdException {
+        if (items.contains(item)) {
+            throw new DuplicateIdException("Item already exists in repository: " + item);
+        }
         items.add(item);
     }
 
-    public void remove(T item) {
-        items.remove(item);
+    public void remove(T item) throws EntityNotFoundException {
+        if (!items.remove(item)) {
+            throw new EntityNotFoundException("Item not found in repository: " + item);
+        }
     }
 
     public List<T> getAll() {
