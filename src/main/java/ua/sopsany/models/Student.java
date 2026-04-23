5416728fec828bca02afed4f8c27afec98935fd8
K1Desktop <avoloschenko008@gmail.com>
2026-04-23 12:00:30 +0300

    lombok at student

--- src/main/java/ua/sopsany/models/Student.java
+++ src/main/java/ua/sopsany/models/Student.java
@@ -1,6 +1,9 @@
 package ua.sopsany.models;
 
 import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
 import ua.sopsany.reflection.Validate;
 
 public final class Student extends Person {
@@ -9,19 +12,31 @@ public final class Student extends Person {
     public enum StudentState {STUDYING, ACADEMIC_LEAVE, DEDUCTED}
 
 
+    @Getter
+    @lombok.Setter
     @Validate(min = 1, max = 6)
     private int course;
 
+    @Getter
+    @Setter
     @Validate(notNull = true, minLength = 1, maxLength = 20)
     private String group;
 
+    @Getter
+    @Setter
     @Validate(min = 1990, max = 2025)
     private int entryYear;
 
+    @Getter
+    @Setter
     @Validate(notNull = true, minLength = 3, maxLength = 20)
     private String studentID;
 
+    @Getter
+    @Setter
     private FormEducation educationForm;
+    @Getter
+    @Setter
     private StudentState studentState;
 
     public Student(String name, String surname, String lastname, LocalDate birthday, String email, String phone, int id,
@@ -37,41 +52,9 @@ public final class Student extends Person {
 
     public Student() {}
 
-    public void setCourse(int course) {
-        this.course = course;
-    }
-
-    public void setGroup(String group) {
-        this.group = group;
-    }
-
-    public void setEntryYear(int entryYear) {
-        this.entryYear = entryYear;
-    }
-
-    public void setStudentID(String studentID) {
-        this.studentID = studentID;
-    }
-
-    public void setEducationForm(FormEducation educationForm) {
-        this.educationForm = educationForm;
-    }
-
-    public void setStudentState(StudentState studentState) {
-        this.studentState = studentState;
-    }
-
     public Student(String surname, String name, String lastname) {
         super(surname, name, lastname);
     }
-
-    public int getCourse() { return course; }
-    public String getGroup() { return group; }
-    public int getEntryYear() { return entryYear; }
-    public String getStudentID() { return studentID; }
-
-    public FormEducation getEducationForm() { return educationForm; }
-    public StudentState getStudentState() { return studentState; }
 
     @Override
     public String toString() {
